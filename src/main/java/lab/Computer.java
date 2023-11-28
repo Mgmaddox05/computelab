833c708c2296fdda19372cb299133591cc34ccf0
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-11-28 15:21:11 -0600

    Assignment 3: Data Structures

---
+++ src/main/java/lab/Computer.java
@@ -0,0 +1,16 @@
+package lab;
+
+public class Computer {
+    private String manufacturer;
+    private String id;
+    private String processer;
+    private String yearBought;
+
+    public Computer(String manufacturer, String id, String processor, String yearBought)
+    {
+        this.manufacturer = manufacturer;
+        this.id = id;
+        this.processer = processor;
+        this.yearBought = yearBought;
+    }
+}
---
+++ src/main/java/lab/Montior.java
@@ -0,0 +1,11 @@
+package lab;
+
+public class Montior {
+    private String manufacturer;
+    private String resolution;
+
+    public Montior(String manufacturer, String resolution){
+        this.manufacturer = manufacturer;
+        this.resolution = resolution;
+    }
+}
